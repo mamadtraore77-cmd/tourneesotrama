149a5d00c0142bcadb8370ee5dd8140844e3cf34
mamadtraore77-cmd <mamad.traore77@gmail.com>
2026-05-15 23:58:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 <script>
-const GOOGLE_SCRIPT_URL = 
-"https://script.google.com/macros/s/AKfycbyoqQPBFc42_hIe-1RvcvgojYiYGE9ie07P70t8WroyrCBBmaapimXeHbrbanWYMFAYow/exec";
+const URL = "https://script.google.com/macros/s/AKfycbyoqQPBFc42_hIe-1RvcvgojYiYGE9ie07P70t8WroyrCBBmaapimXeHbrbanWYMFAYow/exec";
 
 document.getElementById("callForm").addEventListener("submit", function(e) {
-  e.preventDefault(); // ✅ empêche le rechargement
+  e.preventDefault();
 
   const data = new URLSearchParams();
 
@@ -20,10 +19,9 @@ document.getElementById("callForm").addEventListener("submit", function(e) {
     document.getElementById("confirme").checked ? "Oui" : "Non"
   );
 
-  fetch(GOOGLE_SCRIPT_URL, {
+  fetch(URL, {
     method: "POST",
-    body: data,
-    mode: "no-cors"
+    body: data
   });
 
   alert("✅ Appel enregistré !");
